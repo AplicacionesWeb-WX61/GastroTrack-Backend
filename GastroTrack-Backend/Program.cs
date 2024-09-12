f23ee42b22848849a03e42870a2edc64967dda14
MRamirez202210582 <U202210582@upc.edu.pe>
2024-09-11 19:34:37 -0500

    Update Program.cs

--- GastroTrack-Backend/Program.cs
+++ GastroTrack-Backend/Program.cs
@@ -157,7 +157,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
